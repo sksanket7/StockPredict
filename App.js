43afa39ebd2f8a544bf5d17e46ff5a44f16996aa
sanketkarvekar <sksanket7@gmail.com>
2021-01-24 16:20:45 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
+/* import { StatusBar } from 'expo-status-bar'; */
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableNativeFeedback, Button, Alert, Platform, StatusBar} from 'react-native';
 
 export default function App() {
+  let i = 0;
+  let j = 0;
+  const handlePress = () => console.log("Text Pressed"+ i++);
+  //console.log(require("./assets/icon.png"));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={3} onPress={handlePress}>Welcome to StockPredict App Bhai. khbcjdgfjdgfjhdfgdshugf khdskfhsdkjfhksdjhfkj kjhdfkjsdhkfjhdk</Text>
+      <TouchableNativeFeedback onPress={() => console.log("Image Pressed"+ j++)}><Image source={{
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300"}}/>
+        </TouchableNativeFeedback>
       <StatusBar style="auto" />
-    </View>
+      <Button
+      color="orange"
+      title="Press Me" onPress={() => Alert.alert("My Title", "My message", [
+        {text: "Yes", onPress: () => console.log("Yes")},
+        {text: "No", onPress: () => console.log("No")}
+      ])}/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#5DADEC',
+    /* alignItems: 'center',
+    justifyContent: 'center' */
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
